282d09451c188f5352609d5b300c1ab8a3742103
Arthur Santos <asvs@cin.ufpe.br>
2023-08-30 16:39:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var v1 = parseFloat(document.getElementById("1").value);
+var c1 = parseFloat(document.getElementById("2").value);
+var v2 = parseFloat(document.getElementById("3").value);
+var c2 = parseFloat(document.getElementById("4").value);
+var calcular = document.getElementById("calcular");
+var resultado = document.getElementById("11");
+
+calcular.onclick = function() {
+    alert(c2)
+    /*
+    if (v1 == "" & c2 != "" & v2 != "" & c1 != ""){
+      resultado.value = ((v1*c2)/c1)
+    }else if (c1 == "" & c2 != "" & v2 != "" & v1 != ""){
+      resultado.value = ((v1*c2)/c1)
+    }else if (v2 == "" & c2 != "" & c1 != "" & v1 != ""){
+      resultado.value = ((v1*c2)/c1)
+    }else if ((c2 == "") && (c1 != "") && (v2 != "") && (v1 != "")){
+      resultado.value = ((v1*c2)/c1)
+    }  
+    */
+  
+  
+};
